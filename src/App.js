297ea42857cf93c7a817896b42b1b8ce60075996
John Knotts <jhnnyk@gmail.com>
2024-06-08 14:34:53 -0600

    loading robots(users) from an API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 import "./App.css"
 
@@ -8,9 +7,16 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  // lifecycle hook
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
   }
 
   onSearchChange = (event) => {
