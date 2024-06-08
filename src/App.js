576ea9b68a5fe6d5c0ee69e8afa48792062da3c1
John Knotts <jhnnyk@gmail.com>
2024-06-08 14:36:47 -0600

    add a state for loading in case it takes a long time

--- src/App.js
+++ src/App.js
@@ -30,15 +30,18 @@ class App extends Component {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
 
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
-
 }
 
 export default App;
